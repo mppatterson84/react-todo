2c5c7c13be965d6e587d320eeb34cc5bf9b6b4b0
mppatterson84 <mppatterson84@gmail.com>
2021-09-18 09:16:18 -0500

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TodoList from './components/TodoList';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <div className="container">
-        <h1>Todo</h1>
+        <Route exact path="/signup" component={Signup} />
+        {/* <Signup /> */}
+        <Route exact path="/login" component={Login} />
+        {/* <Login /> */}
+        <Route exact path="/" component={TodoList} />
+        {/* <TodoList /> */}
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,31 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Navbar
-        </a>
+        <Link className="navbar-brand" to="/">
+          Todo
+        </Link>
+        <div>
+          <ul className="navbar-nav me-auto mb-2 mb-md-8">
+            <li className="nav-item">
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/logout" className="nav-link">
+                Log Out
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
