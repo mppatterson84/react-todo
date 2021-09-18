dbca2c4ce75d19997d5df4a4104896a1b88edc2b
mppatterson84 <mppatterson84@gmail.com>
2021-09-18 07:17:24 -0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <div className="container">
         <h1>Todo</h1>
       </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Navbar
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
